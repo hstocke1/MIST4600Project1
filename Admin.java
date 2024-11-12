566a0a866d8818f0f098b89af54a11c32a739be7
Owen <owendonnelly325@gmail.com>
2024-11-11 19:35:32 -0500

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -3,12 +3,27 @@ import java.util.*;
 public class Admin extends User {
 	private AccountManager accountManager;
 	
-	public Admin(String username, String rawPass, String firstName, String lastName) {
+	public Admin(String username, String rawPass, String firstName, String lastName, AccountManager accountManager) {
 		super(username, rawPass, firstName, lastName);
+		this.accountManager = accountManager;
 	}
 	
 	public ArrayList<User> getUsers() {
 		return new ArrayList<>(accountManager.users.values());
     }
+
+	public void printUsers() {
+	    ArrayList<User> users = getUsers();
+	    for (User user : users) {
+	        System.out.println("Username: " + user.getUsername() + ", Name: " + user.getName());
+	    }
+	}
 	
+	public boolean changePassword(User user, String newPass) {
+		if (user != null) {
+			user.changePassword(newPass);
+			return true;
+		}
+		return false;
+	}
 }
