e5ccd7a69ad984ce80049ee9c112a7eedaaac8be
Owen <owendonnelly325@gmail.com>
2024-11-12 17:29:12 -0500

    Update AccountManager.java

--- AccountManager.java
+++ AccountManager.java
@@ -3,10 +3,12 @@ import java.util.*;
 public class AccountManager {
 	protected HashMap<String, User> users; //stores user 
 	private HashSet<String> usernames; //keep track of usernames, avoid duplicates
+	private TransactionHistory transactionHistory; //universal transaction tracking
 	
 	public AccountManager() {
 		this.users = new HashMap<>();
         this.usernames = new HashSet<>();
+        this.transactionHistory = new TransactionHistory();
 	}
 	
 	public boolean createAccount(String username, String rawPass, String fName, String lName, boolean isAdmin) {
@@ -14,21 +16,18 @@ public class AccountManager {
 			System.out.println("A user with the name: " + username + " already exists.");
 			return false;
 		} else {
-			
 			if (isAdmin) {
 				Admin newAdmin;
 				newAdmin = new Admin(username, rawPass, fName, lName, this);
 				users.put(username, newAdmin);
 	        } else {
 	        	User newUser;
-	            newUser = new User(username, rawPass, fName, lName);
+	            newUser = new User(username, rawPass, fName, lName, this);
 	            users.put(username, newUser);
 	        }
-			
 			usernames.add(username);
 			return true;
 		}
-		
 	}
 	
 	public User logIn(String username, String rawPass) {
@@ -44,4 +43,15 @@ public class AccountManager {
 		return null; //login failed
 	}
 	
+	public User getUser(String username) {
+		User user = users.get(username);
+		return user;
+	}
+	
+	public void addTransaction(Transaction transaction) {
+		transactionHistory.addTransaction(transaction);
+	}
+	public ArrayList<Transaction> getAllTransactions() {
+        return transactionHistory.getAllTransactions();
+    }
 }
