7ce1452c583d7708784c9ea0a0b8ca3eeb4d9e11
Owen <owendonnelly325@gmail.com>
2024-11-12 17:30:03 -0500

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -2,9 +2,10 @@ import java.util.*;
 
 public class Admin extends User {
 	private AccountManager accountManager;
+	private TransactionHistory transactionHistory;
 	
 	public Admin(String username, String rawPass, String firstName, String lastName, AccountManager accountManager) {
-		super(username, rawPass, firstName, lastName);
+		super(username, rawPass, firstName, lastName, accountManager);
 		this.accountManager = accountManager;
 	}
 	
@@ -12,18 +13,33 @@ public class Admin extends User {
 		return new ArrayList<>(accountManager.users.values());
     }
 
-	public void printUsers() {
-	    ArrayList<User> users = getUsers();
-	    for (User user : users) {
-	        System.out.println("Username: " + user.getUsername() + ", Name: " + user.getName());
-	    }
-	}
-	
 	public boolean changePassword(User user, String newPass) {
 		if (user != null) {
 			user.changePassword(newPass);
 			return true;
 		}
 		return false;
+	}
+	
+	public void printMyTransactions() {
+        for (Transaction transaction : transactionHistory.getAllTransactions()) {
+            System.out.println(transaction);
+        }
+	}
+	
+	@Override
+	public void printAllTransactions() {
+        ArrayList<Transaction> transactions = accountManager.getAllTransactions();
+        for (Transaction transaction : transactions) {
+            System.out.println(transaction);
+        }
+	}
+	
+	@Override
+	public void printUsers() {
+	    ArrayList<User> users = getUsers();
+	    for (User aUser : users) {
+	        System.out.println("Username: " + aUser.getUsername() + ", Name: " + aUser.getName());
+	    }
 	}
 }
