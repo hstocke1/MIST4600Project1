ec96d2e4c98bce5e983e4d2e091917c2de759108
Owen <owendonnelly325@gmail.com>
2024-11-15 11:13:38 -0500

    Update LoginManager.java

--- LoginManager.java
+++ LoginManager.java
@@ -1,17 +1,15 @@
 public class LoginManager {
-
     public static void main(String[] args) {
         AccountManager accManager = new AccountManager();
         UserInterface userInterface = new UserInterface(accManager);
         
         accManager.createAccount("admin", "password", "Admin", null, true);
-        accManager.createAccount("odonnelly", "myPass", "Owen", "Donnelly", false);
-        accManager.createAccount("hstocke", "hisPass", "Hank", "Stocke", false);
-        accManager.createAccount("lhelm", "pass", "Levon", "Helm", false);
-        accManager.createAccount("rmanuel", "pass", "Richard", "Manuel", false);
-        accManager.createAccount("gbrooks", "pass", "Garth", "Brooks", false);
+        accManager.createAccount("rdanko", "bass", "Rick", "Danko", false);
+        accManager.createAccount("rrobertson", "guitar", "Robbie", "Robertson", false);
+        accManager.createAccount("lhelm", "drums", "Levon", "Helm", false);
+        accManager.createAccount("rmanuel", "piano", "Richard", "Manuel", false);
+        accManager.createAccount("gbrooks", "organ", "Garth", "Brooks", false);
     
         userInterface.start();
-    
     }
 }
