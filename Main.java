a81a7faa941b8d96beaecad771648106c5caee92
Owen <owendonnelly325@gmail.com>
2024-11-15 12:13:05 -0500

    Update and rename LoginManager.java to Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-public class LoginManager {
+public class Main {
     public static void main(String[] args) {
         AccountManager accManager = new AccountManager();
         UserInterface userInterface = new UserInterface(accManager);
